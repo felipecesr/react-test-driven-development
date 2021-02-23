1f29afe73a2536a01ffb030cc838e8f361f2b6ae
Felipe César <felipecesr@gmail.com>
2021-02-23 17:17:05 -0300

    chore: remove initial component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import Expense from 'components/Expense'
-
 function App() {
   return (
     <div>
-      <h1>Expenses</h1>
-      <Expense title="Spotify" value="15,60" paid={false} />
+      <h1>My app</h1>
     </div>
   )
 }
