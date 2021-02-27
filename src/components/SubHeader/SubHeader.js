bb435cd7e0a7c308df661f5d4586267ff978774c
Felipe César <felipecesr@gmail.com>
2021-02-27 13:26:04 -0300

    chore: remove sub-header

--- src/components/SubHeader/SubHeader.js
+++
@@ -1,7 +0,0 @@
-const SubHeader = ({ title }) => (
-  <div>
-    <h2>{title}</h2>
-  </div>
-)
-
-export default SubHeader
--- src/components/SubHeader/__tests__/SubHeader.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import SubHeader from '../SubHeader'
-
-test('renders the title', () => {
-  render(<SubHeader title='My Expenses' />)
-  expect(
-    screen.getByRole('heading', { name: /my expenses/i })
-  ).toBeInTheDocument()
-})
