88301ff835a1b05d84993f3910f41bbaa068f949
Felipe César <felipecesr@gmail.com>
2021-03-16 10:40:58 -0300

    chore: rename no match page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Switch, Route } from 'react-router-dom'
 import GlobalStyles from 'styles/global'
 import Home from 'pages/Home/Home'
 import Form from 'pages/Form/Form'
-import Error from 'pages/Error/Error'
+import NoMatch from 'pages/NoMatch/NoMatch'
 
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={Home} />
         <Route path='/new' component={Form} />
-        <Route component={Error} />
+        <Route component={NoMatch} />
       </Switch>
     </>
   )
